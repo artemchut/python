4963ccf925ece0b2fe151db4e00bc012876dec1e
Artem Chut <162903446+artemchut@users.noreply.github.com>
2025-09-16 07:17:57 +1300

    wordle.py

--- wordle.py
+++ wordle.py
@@ -1,11 +1,13 @@
 word="hello"
 guesses=0
 correct=0
+won=False
 guessed = ["-", "-", "-", "-", "-"]
-while guesses < 5:
+while guesses < 5 and won != True:
     guess=input("Enter a 5 letter word: ")
     if guess==word:
         print("You won!!")
+        won=True
     while len(guess) != 5:
         print("It has to be 5 letters")
         guess=input("Enter a 5 letter word: ")
@@ -17,5 +19,6 @@ while guesses < 5:
     print("".join(guessed))
     if correct==0:
         print("Unfortunately you got 0 correct")  
-print("Unfortunately all of your 5 guesses were used up")  
-print("You lost")  
+if guesses==5:        
+    print("Unfortunately all of your 5 guesses were used up")  
+    print("You lost")  
