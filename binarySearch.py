9ee44d900d14a767e706bb623c600445dd6ce8ad
Artem Chut <162903446+artemchut@users.noreply.github.com>
2025-12-02 16:22:01 +0000

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,5 +1,14 @@
-numList = sorted(list("0123456789"))
-needsFound = "7"
+numList = [10,9,8,7,6,5,4,3,2,1,0]
+swapped=True
+while swapped:
+    swapped=False
+    for i in range(0,len(numList)-1):
+        if numList[i]>numList[i+1]:
+            smallest=numList[i+1]
+            numList[i+1]=numList[i]
+            numList[i]=smallest
+            swapped=True
+needsFound = 7
 found = False
 
 while not found:
