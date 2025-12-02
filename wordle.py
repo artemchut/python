368406a2574f8f4c2756e8692a91494c007f894c
Artem Chut <162903446+artemchut@users.noreply.github.com>
2025-12-02 16:10:56 +0000

    Update wordle.py
    Added functionality to indicate letters that are in the word but in the wrong position.

--- wordle.py
+++ wordle.py
@@ -1,24 +1,24 @@
 word="hello"
 guesses=0
-correct=0
 won=False
 guessed = ["-", "-", "-", "-", "-"]
+print("* means that the letter is in the word but in a different place")
 while guesses < 5 and won != True:
     guess=input("Enter a 5 letter word: ")
-    if guess==word:
-        print("You won!!")
-        won=True
     while len(guess) != 5:
         print("It has to be 5 letters")
         guess=input("Enter a 5 letter word: ")
     guesses += 1
-    for i in range(0,5):
+    if guess==word:
+        print("You won!!")
+        won=True
+        continue
+    for i in range(0,len(word)):
         if guess[i] == word[i]:
             guessed[i] = word[i]  
-            correct +=1
+        elif guess[i] in word and guess[i]!=word[i] and guess.count(guess[i])<=word.count(guess[i]):
+            guessed[i]="*"
     print("".join(guessed))
-    if correct==0:
-        print("Unfortunately you got 0 correct")  
 if guesses==5:        
     print("Unfortunately all of your 5 guesses were used up")  
     print("You lost")  
