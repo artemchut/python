2ec232ac1ace6c61a6799a796948a0e7ddc1e2ca
artemchut <artemchut2022@gmail.com>
2026-03-13 22:30:43 +0000

    lossy_compression

---
+++ lossy_compression.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+image = Image.open("image.jpg")
+pixels = image.load()
+width, height = image.size
+
+length = 2
+
+new_image = Image.new("RGB", (width // length, height // length))
+new_pixels = new_image.load()
+
+new_x = 0
+new_y = 0
+for y in range(0, height-1, length):
+    for x in range(0, width-1, length):
+        r = g = b = 0
+        r = pixels[x,y][0] + pixels[x+1,y][0] + pixels[x,y+1][0] + pixels[x+1,y+1][0]
+        g = pixels[x,y][1] + pixels[x+1,y][1] + pixels[x,y+1][1] + pixels[x+1,y+1][1]
+        b = pixels[x,y][2] + pixels[x+1,y][2] + pixels[x,y+1][2] + pixels[x+1,y+1][2]
+
+        avg = (r // (length*length), g // (length*length), b // (length*length))
+        new_pixels[new_x, new_y] = avg
+        new_x += 1
+    new_x = 0
+    new_y += 1
+
+new_image.save("new_image.jpg")
