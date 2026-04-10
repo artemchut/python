859c4390bcf81d18432cc01ed6313c27b7463a62
Artem Chut <162903446+artemchut@users.noreply.github.com>
2026-04-10 20:24:21 +0100

    Update wordSearch.py

--- wordSearch.py
+++ wordSearch.py
@@ -1,81 +1,143 @@
 import random
-letters="abcdrfghijklmnopqrstuvwxyz"
-word="hello"
-scramble=[[],[],[],[],[],[]]
-charsUsed=0
-found=1
 
-#   CREATES THE WORDSEARCH TABLE ITSELF   #
-for j in range(0,12):
-        scramble[0]+=random.choice(letters)
-for i in range(1,len(scramble)):
-    for k in range(3):
-        scramble[i]+=random.choice(letters)
-    scramble[i]+=word[charsUsed]
-    scramble[i][2]=word[charsUsed]
-    for k in range(8):
-        scramble[i]+=random.choice(letters)
-    
-    charsUsed+=1
-scramble[5][2]="x"
-scramble[4]="xhelloxxxxxx"
+letters = "abcdefghijklmnopqrstuvwxyz"
+words_to_be_found = ["hello", "eye"]
 
-for i in scramble:
-    print("".join(i))
-    
+row_width = 8
 
-#  LOOKS AT EVERY ROW AND COLUMN
-for i in range(0,len(scramble)):
-    for k in range(0,len(scramble[i])):
-        if scramble[i][k]==word[0]:
-            #   LOOKS FOR THE ADJACENT LETTERS, IF OUT OF BOUND - continue
-            try:
-                for j in range(1,len(word)):
-                    if scramble[i+j][k]==word[j]:
-                        found+=1
-                    # RESETS THE COUNTER IF SOME LETTERS ARE FOUND BUT NOT THE FULL WORD
-                    else:
-                        found=1
-                        break
-                    if found==len(word):
-                        print(f"Start at index {i}:{k}, direction: down")
-                        break
-                        
-                found=1
-                for j in range(1,len(word)):
-                    if scramble[i-j][k]==word[j]:
-                        found+=1
-                    # RESETS THE COUNTER IF SOME LETTERS ARE FOUND BUT NOT THE FULL WORD
-                    else:
-                        found=1
-                        break
-                    if found==len(word):
-                        print(f"Start at index {i}:{k}, direction: up")
-                        break
-                        
-                found=1
-                for j in range(1,len(word)):
-                    if scramble[i][k+j]==word[j]:
-                        found+=1
-                    # RESETS THE COUNTER IF SOME LETTERS ARE FOUND BUT NOT THE FULL WORD
-                    else:
-                        found=1
-                        break
-                    if found==len(word):
-                        print(f"Start at index {i}:{k}, direction: right")
-                        break
-                        
-                found=1
-                for j in range(1,len(word)):
-                    if scramble[i][k-j]==word[j]:
-                        found+=1
-                    # RESETS THE COUNTER IF SOME LETTERS ARE FOUND BUT NOT THE FULL WORD
-                    else:
-                        found=1
-                        break
-                    if found==len(word):
-                        print(f"Start at index {i}:{k}, direction: left")
-                        break
-                        
-            except:
-                continue
+letters_found = 1
+
+puzzle = [[],[],[],[],[],[]]
+# GENERATION OF THE PUZZLE
+for row in range(len(puzzle)):
+    for i in range(row_width):
+        puzzle[row] += random.choice(letters)
+            
+# SHOWING THAT IT WORKS IN EVERY DIRECTION
+# right
+puzzle[0][2] = "h"
+puzzle[0][3] = "e"
+puzzle[0][4] = "l"
+puzzle[0][5] = "l"
+puzzle[0][6] = "o"
+# left
+puzzle[5][2] = "o"
+puzzle[5][3] = "l"
+puzzle[5][4] = "l"
+puzzle[5][5] = "e"
+puzzle[5][6] = "h"
+# up left
+puzzle[4][5] = "e"
+puzzle[3][4] = "l"
+puzzle[2][3] = "l"
+puzzle[1][2] = "o"
+# down right
+puzzle[1][3] = "e"
+puzzle[2][4] = "l"
+puzzle[3][5] = "l"
+puzzle[4][6] = "o"
+# down left
+puzzle[0][7] = "h"
+puzzle[1][6] = "e"
+puzzle[2][5] = "l"
+puzzle[3][4] = "l"
+puzzle[4][3] = "o"
+# down
+puzzle[1][7] = "e"
+puzzle[2][7] = "l"
+puzzle[3][7] = "l"
+puzzle[4][7] = "o"
+# up
+puzzle[1][0] = "o"
+puzzle[2][0] = "l"
+puzzle[3][0] = "l"
+puzzle[4][0] = "e"
+# up right
+puzzle[5][0] = "h"
+puzzle[4][1] = "e"
+puzzle[3][2] = "l"
+puzzle[2][3] = "l"
+puzzle[1][4] = "o"
+# up right and down left
+puzzle[3][1] = "y"
+puzzle[2][2] = "e"
+
+for row in puzzle:
+    print(row)
+
+for word in words_to_be_found: # checking for each word that needs to be found
+    for i in range(len(puzzle)):
+        for k in range(len(puzzle[i])):
+            if puzzle[i][k] == word[0] or puzzle[i][k] == word[len(word)-1]:
+                # RIGHT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i][k+letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: right, word: {word}")
+                letters_found = 1
+                
+                # DOWN
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i+letters_found][k]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: down, word: {word}")
+                letters_found = 1
+                
+                # DOWN RIGHT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i+letters_found][k+letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: down-right, word: {word}")
+                letters_found = 1
+                
+                # DOWN LEFT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i+letters_found][k-letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: down-left, word: {word}")
+                letters_found = 1
+
+                # LEFT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i][k-letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: left, word: {word}")
+                letters_found = 1
+                
+                # UP
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i-letters_found][k]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: up, word: {word}")
+                letters_found = 1
+                
+                # UP LEFT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i-letters_found][k-letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: up-left, word: {word}")
+                letters_found = 1
+                
+                # UP RIGHT
+                try:
+                    while letters_found < len(word) and word[letters_found] == puzzle[i-letters_found][k+letters_found]:
+                        letters_found += 1
+                except:pass
+                if letters_found == len(word):
+                    print(f"Start index: {i},{k}, direction: up-right, word: {word}")
+                letters_found = 1
+                
